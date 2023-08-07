d003e89ebc80c831d66de211d9101061674e7294
Vladyslav <122233433+tassarian@users.noreply.github.com>
2023-08-07 20:12:31 +0300

    schedule-and-mediaQuery
    done

--- src/components/Balance/Balance.jsx
+++ src/components/Balance/Balance.jsx
@@ -1,3 +1,4 @@
+import { useMediaQuery } from 'react-responsive';
 import {
   StyledBalance,
   StyledBalanceBox,
@@ -5,12 +6,27 @@ import {
 } from './BalanceStyled';
 
 export const Balance = () => {
+  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1279 });
+  const isDesktop = useMediaQuery({ minWidth: 1280 });
+
   return (
-    <StyledBalanceBox>
-      <StyledBalanceLabel>Your balance</StyledBalanceLabel>
-      <StyledBalance>
-        <span>₴</span>24 000.00
-      </StyledBalance>
-    </StyledBalanceBox>
+    <div>
+      {isDesktop && (
+        <StyledBalanceBox>
+          <StyledBalanceLabel>Your balance</StyledBalanceLabel>
+          <StyledBalance>
+            <span>₴ </span>24 000.00
+          </StyledBalance>
+        </StyledBalanceBox>
+      )}
+      {isTablet && (
+        <StyledBalanceBox maxW="296px" padL="40px" borR="8px" marB="0px">
+          <StyledBalanceLabel>Your balance</StyledBalanceLabel>
+          <StyledBalance>
+            <span>₴ </span>24 000.00
+          </StyledBalance>
+        </StyledBalanceBox>
+      )}
+    </div>
   );
 };
