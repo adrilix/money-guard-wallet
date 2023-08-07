f7f5ba7091f9c53af50eb1a1d827047ab7df064e
Andrii Maliarenko <106884639+adrilix@users.noreply.github.com>
2023-08-07 22:41:23 +0200

    add one selector

--- src/redux/registrationReducer/registrationThunks.js
+++ src/redux/registrationReducer/registrationThunks.js
@@ -88,5 +88,6 @@ export const refreshAuthThunk = createAsyncThunk(
 );
 
 export const selectAuthData = state => state.auth.userData;
+export const selectAuthDataBalance = state => state.auth.userData.balance;
 export const selectAuthIsLoadingStatus = state => state.auth.isLoading;
 export const selectAuthErrorStatus = state => state.auth.error;
