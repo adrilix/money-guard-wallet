b8e72c66f3badb548fc04c11bb01294fd640fa1f
ururh <58848421+ururh@users.noreply.github.com>
2023-08-07 14:08:49 +0300

    some style, header modal

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { Box, ExitButton, ExitIcon, ExitText, HeaderContainer, Logo, LogoBox, LogoName, Name, Stick } from './HeaderStyled';
-import logo from '../../svg/logo.svg'
-import exit from '../../svg/exit.svg'
+import logo from '../../svg/logo.svg';
+import exit from '../../svg/exit.svg';
+import { useDispatch, useSelector } from 'react-redux';
+import { logOutThunk, selectAuthData } from 'redux/registrationReducer/registrationThunks';
+import { LogOutModal } from 'components/LogOutModal/LogOutModal';
+
 function Header() {
+  // const name= useSelector(selectAuthData);
+const [showIt, setShowIt] = useState(false);
+  const dispatch = useDispatch();
+
+  const showLogOutModal = () => {
+    setShowIt(true);
+  };
+
+  const handleLogOut = () => {
+    dispatch(logOutThunk());
+    setShowIt(false);
+  };
+
+
   return (
-    <HeaderContainer>
+   <> <HeaderContainer>
       <LogoBox>
         <Logo src={logo} alt="logo" />
         <LogoName>Money Guard</LogoName>
       </LogoBox>
       <Box>
-        <Name>Name</Name>
+        {/* {name && <Name></Name>} */}
         <Stick />
-        <ExitButton id="exit" type="button">
+        <ExitButton id="exit" type="button" onClick={showLogOutModal}>
           <ExitIcon src={exit} alt="exit" />
           <ExitText>Exit</ExitText>
         </ExitButton>
       </Box>
     </HeaderContainer>
+    <LogOutModal showIt={showIt} setShowIt={setShowIt} handleLogOut={handleLogOut} /></>
   );
 }
 
