6428bbc259464d520fd884f383514c60a3b52461
ururh <58848421+ururh@users.noreply.github.com>
2023-08-09 10:22:59 +0300

    color list

---
+++ src/Page/SummaryPage/colorStatistic.js
@@ -0,0 +1,11 @@
+export const colorStatistics = [
+    {name: 'Main expenses', color: '#FED057'},
+    { name: 'Products', color:'#FFD8D0'},
+    {name: 'Car',color:'#FD9498'},
+    {name: 'Self care',color:'#C5BAFF'},
+    {name:'Child care',color:'#6E78E8'},
+    {name: 'Household products',color:'#4A56E2'},
+    {name:'Education',color:'#81E1FF'},
+    {name:'Leisure',color:'#24CCA7'},
+    {name:'Other expenses',color:'#00AD84'}
+]
